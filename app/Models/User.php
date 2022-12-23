a85de4b18326c2cd94b1c580dde2d5ce320465b8
ValeriyLiubchenko <valeriyliubchenko98@gmail.com>
2022-12-23 13:12:38 +0200

    1

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    private function posts()
+    {
+        return $this->hasMany(Post::Class);
+    }
 }
